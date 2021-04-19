71de65e0e680c4d13f4e291d5157a17346360452
Shanta-Dutta <74420422+Shanta-Dutta@users.noreply.github.com>
2021-04-19 16:01:44 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+package com.tts.calculator;
+import java.util.*;
+
+//class calculator implementing basic functionality
+ public class Calculator
+{
+
+    //add method
+    public int add (int num1, int num2)
+    {
+
+        return num1 + num2;
+    }
+    //subtract method
+    public int subtract (int num1, int num2)
+    {
+        return num1 - num2;
+
+    }
+    //multiply method
+    public int multiply (int num1, int num2)
+    {
+        return num1 * num2;
+    }
+    //divide method
+    public int divide (int num1, int num2)
+    {
+
+        if (num2 == 0)
+        {
+            System.out.println ("num2 cannot be zero");
+            return 0;
+        }
+        else
+            return num1 / num2;
+    }
+
+    //square method
+    public int square (int num1)
+    {
+        return num1 * num1;
+    }
+}
+
